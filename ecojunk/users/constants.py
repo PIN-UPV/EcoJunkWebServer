628478008a0aed2eb9c4119e8257d67a751cad0d
Iván Martín <ivanmartinga@gmail.com>
2018-10-02 14:23:03 +0200

    wip

---
+++ ecojunk/users/constants.py
@@ -0,0 +1,5 @@
+from django.utils.translation import ugettext_lazy as _
+
+USER = "user"
+RIDER = "rider"
+ROL_TYPES = [(USER, _("User")), (RIDER, _("rider"))]
--- ecojunk/users/models.py
+++ ecojunk/users/models.py
@@ -1,14 +1,31 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.contrib.gis.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
+from ecojunk.users.constants import ROL_TYPES
+
 
 class User(AbstractUser):
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    permissions = models.ManyToManyField(
+        "users.Permission", verbose_name=_("permissions")
+    )
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
 
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
+    def __str__(self):
+        return self.name
+
+
+class Permission(mode):
+    rol = models.CharField(_("Rol"), choices=ROL_TYPES, max_length=255)
+
+    class Meta:
+        verbose_name = _("Permission")
+        verbose_name_plural = _("Permissions")
+
+    def __str__(self):
+        return self.rol
