a43166ebb793b11a89badbe57af868f9b628c26a
Iván Martín <ivanmartinga@gmail.com>
2018-10-10 09:23:32 +0200

    Models (#3)

---
+++ ecojunk/junk/models.py
@@ -0,0 +1,76 @@
+from django.contrib.gis.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import ugettext_lazy as _
+
+
+class Deal(models.Model):
+    customer = models.ForeignKey("users.User", verbose_name=_("Customer"))
+    rider = models.ForeignKey("users.User", verbose_name=_("Rider"))
+
+    junk_point = models.ForeignKey(
+        "junk.JunkPoint", verbose_name=_("Junk point"), null=True
+    )
+
+    date = models.DateTimeField(_("Date"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Deal")
+        verbose_name = _("Deals")
+
+    def __str__(self):
+        return self.date
+
+
+class Trash(models.Model):
+    deal = models.ForeignKey("junk.Deal", verbose_name=_("Deal"))
+
+    name = models.CharField(_("Name"), max_length=255)
+    description = models.TextField(_("Description"), max_length=512)
+
+    type = models.ForeignKey("junk.TrashType", verbose_name=_("Trash type"))
+
+    class Meta:
+        verbose_name = _("Trash")
+        verbose_name = _("Trash")
+
+    def __str__(self):
+        return self.name
+
+
+class TrashType(models.Model):
+    name = models.CharField(_("Name"), max_length=255)
+
+    class Meta:
+        verbose_name = _("Trash type")
+        verbose_name = _("Trash type")
+
+    def __str__(self):
+        return self.name
+
+
+class JunkPoint(models.Model):
+    location = models.PointField(_("Location"))
+    type = models.ForeignKey("junk.JunkPoint", verbose_name=_("Junk point"))
+
+    contract = models.ForeignKey(
+        "companies.Contract", verbose_name=_("Contract"), null=True
+    )
+
+    class Meta:
+        verbose_name = _("Junk point")
+        verbose_name = _("Junk points")
+
+    def __str__(self):
+        return self.location
+
+
+class JunkPointType(models.Model):
+    name = models.CharField(_("Name"), max_length=255)
+    description = models.TextField(_("Description"), max_length=512)
+
+    class Meta:
+        verbose_name = _("Junk point type")
+        verbose_name = _("Junk point types")
+
+    def __str__(self):
+        return self.name
--- ecojunk/users/models.py
+++ ecojunk/users/models.py
@@ -1,14 +1,33 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.urls import reverse
+from django.contrib.gis.db import models
 from django.utils.translation import ugettext_lazy as _
+
+from ecojunk.users.constants import ROL_TYPES
 
 
 class User(AbstractUser):
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    permissions = models.ManyToManyField(
+        "users.Permission", verbose_name=_("Permissions")
+    )
+    completed_missions = models.ManyToManyField(
+        "rewards.Mission", verbose_name=_("Completed missions")
+    )
 
-    # First Name and Last Name do not cover name patterns
-    # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
 
-    def get_absolute_url(self):
-        return reverse("users:detail", kwargs={"username": self.username})
+    def __str__(self):
+        return self.name
+
+
+class Permission(models.Model):
+    rol = models.CharField(_("Rol"), choices=ROL_TYPES, max_length=255)
+
+    class Meta:
+        verbose_name = _("Permission")
+        verbose_name_plural = _("Permissions")
+
+    def __str__(self):
+        return self.rol
