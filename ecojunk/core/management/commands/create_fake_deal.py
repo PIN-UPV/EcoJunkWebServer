64d233f054a94b6cc344c3c3301027512ace09b0
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2018-11-28 13:23:20 +0100

    Added automatic state change of deals

---
+++ ecojunk/core/management/commands/create_fake_deal.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from ecojunk.junk.models import Deal
+from ecojunk.junk.constants import ACCEPTED
+import datetime
+
+
+class Command(BaseCommand):
+    help = "Menage the deal deadlines"
+
+    def handle(self, *args, **options):
+        deals = Deal.objects.filter(state=ACCEPTED, accepted_date__gt=datetime.datetime.now()+datetime.timedelta(1, 0, 0, 0, 0, 0, 0))
+        print(str(deals))
+
+
